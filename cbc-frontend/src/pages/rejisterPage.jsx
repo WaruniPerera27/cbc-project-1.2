31bd149eab012ebbc5a92b5d5f1256328393cd34
WaruniPerera27 <waruni.1127@gmail.com>
2025-07-16 10:18:21 +0530

    Day 15-new

--- cbc-frontend/src/pages/rejisterPage.jsx
+++
@@ -1,9 +0,0 @@
-export default function RegisterPage() {
-
-    return (
-        <div>
-            <span className="text-4xl font-bold text-gray-800">Register Page</span>
-        </div>
-    );
-       
-}
